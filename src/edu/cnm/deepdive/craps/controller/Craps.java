ef4d21d8197a5550ae91b8ca00f2f5a8656a69de
kanyon <kanyonwyman@gmail.com>
2019-02-05 08:47:04 -0700

    added mersan twister

--- src/edu/cnm/deepdive/craps/controller/Craps.java
+++ src/edu/cnm/deepdive/craps/controller/Craps.java
@@ -1,28 +1,37 @@
-
 package edu.cnm.deepdive.craps.controller;
 
     import edu.cnm.deepdive.craps.model.Game;
     import edu.cnm.deepdive.craps.model.State;
     import java.util.Random;
     import java.util.Scanner;
+    import org.apache.commons.rng.simple.JDKRandomBridge;
+    import org.apache.commons.rng.simple.RandomSource;
 
 public class Craps {
 
+  private static final int SEED_SIZE = 312;
   private Random rng;
   private Game game;
 
   public static void main(String[] args) {
-    boolean playAgain;
     Craps craps = new Craps();
-    try (Scanner scanner = new Scanner(System.in)) {
-      do {
-        craps.play(true);
-      } while (craps.playAgain(scanner));
+    if (args.length > 0) {
+      int numTrials = Integer.parseInt(args[0]);
+      for (int i = 0; i < numTrials; i ++) {
+        craps.play(false);
+      }
+      craps.displayTally();
+    } else {
+      try (Scanner scanner = new Scanner(System.in)) {
+        do {
+          craps.play(true);
+        } while (craps.playAgain(scanner));
+      }
     }
   }
 
   public Craps() {
-    rng = new Random();
+    rng = new JDKRandomBridge(RandomSource.MT_64, RandomSource.createLongArray(SEED_SIZE));
     game = new Game(rng);
   }
 
@@ -34,11 +43,15 @@ public class Craps {
         System.out.printf("%d:%d%n", diceRoll[0], diceRoll[1]);
       }
       System.out.printf("Outcome: %s%n", state);
-      long wins = game.getWins();
-      long total = wins + game.getLosses();
-      double percentage = 100.0 * wins / total;
-      System.out.printf("Winning percentage = (%d/%d) = %.2f%%%n", wins, total, percentage);
+      displayTally();
     }
+  }
+
+  private void displayTally() {
+    long wins = game.getWins();
+    long total = wins + game.getLosses();
+    double percentage = 100.0 * wins / total;
+    System.out.printf("Winning percentage = (%d/%d) = %.2f%%%n", wins, total, percentage);
   }
 
   private boolean playAgain(Scanner scanner) {
